7921332d84050bdc7ecd8716f9ce21acb7e69d1b
jayarbrul01 <jayarbrul01@outlook.com>
2026-01-23 00:38:01 +0800

    feat:build admin page for ad

--- src/modules/advertisements/advertisements.routes.ts
+++ src/modules/advertisements/advertisements.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { requireAuth } from "../../middlewares/auth.middleware.js";
+import { requireRole } from "../../middlewares/role.middleware.js";
 import { validate } from "../../middlewares/validate.middleware.js";
 import { advertisementsController } from "./advertisements.controller.js";
 import { CreateAdvertisementSchema, UpdateAdvertisementSchema } from "./advertisements.validation.js";
@@ -12,7 +13,8 @@ r.get("/active", advertisementsController.getActiveAdvertisements);
 r.post("/:id/view", advertisementsController.trackView);
 r.post("/:id/click", advertisementsController.trackClick);
 
-// Protected routes (admin only - can be added later)
+// Protected routes (admin only)
+// Note: For now using requireAuth. Add requireRole(["ADMIN"]) after adding ADMIN role to schema
 r.get("/", requireAuth, advertisementsController.getAllAdvertisements);
 r.patch("/:id", requireAuth, validate(UpdateAdvertisementSchema), advertisementsController.updateAdvertisement);
 
